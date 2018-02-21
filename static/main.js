d439e968c86531a9f0871d5c18a6dd36a87643f9
Nikolett Lakos <lakosnikolett@gmail.com>
2018-02-22 00:48:21 +0100

    Cool table style :)

--- static/main.js
+++ static/main.js
@@ -1,9 +1,17 @@
+var $items = $('#datas');
+
 $.ajax({
     dataType: "json",
     url: 'https://swapi.co/api/planets/?page=',
     success: function(datas) {
         $.each(datas.results, function(i, data){
-            console.log(data)
+         $items.append('<tr><td>' + data.name + '</td>' +
+                          '<td>' + data.diameter + ' km' + '</td>' +
+                          '<td>' + data.climate + '</td>' +
+                          '<td>' + data.terrain + '</td>' +
+                          '<td>' + data.surface_water + '</td>' +
+                          '<td>' + data.population + '</td>'
+            );
         })
     }
 });

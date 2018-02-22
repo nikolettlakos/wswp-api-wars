cf318314895d6549b452f4144cec0fe44e01a85e
Nikolett Lakos <lakosnikolett@gmail.com>
2018-02-23 00:36:44 +0100

    Registration: sign in.

--- server.py
+++ server.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template, redirect, session, request
+from flask import Flask, render_template, redirect, session, request, escape, url_for
 app = Flask(__name__)
 app.secret_key = "A0Zr98j/3yX R~XHH!jmN]LWX/,?RT"
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if 'username' in session:
+        user_name = session['username']
+        return render_template("index.html", user_name=user_name)
+    else:
+        return render_template("index.html")
 
 
 @app.route("/registration")
@@ -13,11 +17,18 @@ def registration():
     return render_template("registration.html")
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return render_template("login.html")
+
+
 @app.route('/logout')
 def logout():
     session.pop('username', None)
-    session.pop('user_id', None)
-    return redirect('/')
+    return redirect(url_for('index'))
 
 
 if __name__ == "__main__":

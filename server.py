3197f21d30b6e5d3d273de45554189574e0347ec
Nikolett Lakos <lakosnikolett@gmail.com>
2018-02-22 23:01:09 +0100

    Registration base.

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, session, request
 app = Flask(__name__)
-app.secret_key = "Logan"
+app.secret_key = "A0Zr98j/3yX R~XHH!jmN]LWX/,?RT"
 
 
 @app.route("/")
@@ -8,8 +8,20 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/registration")
+def registration():
+    return render_template("registration.html")
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    session.pop('user_id', None)
+    return redirect('/')
+
+
 if __name__ == "__main__":
-    app.secret_key = "Logan"
+    app.secret_key = "A0Zr98j/3yX R~XHH!jmN]LWX/,?RT"
     app.run(
         debug=True,
         port=5000
